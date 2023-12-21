96356b970b5473d08f8e815ad6bfa2fcc62388b9
Alessx4251 <mr.alesso05@gmail.com>
2023-12-21 20:55:11 -0300

    mejora estetica

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 function Header() {
     return (
-        <div className='flex items-center bg-white rounded-xl shadow-lg text-slate-700 font-medium'>
+        <div className='md:flex bg-white rounded-xl shadow-2xl text-slate-700 font-medium justify-between space-x-5 p-5'>
             <h1>
                 MovieHOOD
             </h1>
+            <p className='flex-1 w-24'>darkmode</p>
+            <button className='flex-1 w-24 h-12 bg-lime-400'>
+                Mis Favoritas
+            </button>
         </div>
     )
 }
